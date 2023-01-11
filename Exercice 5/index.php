21742574d413d6f5c5c106cfbbfe61a6a1d54601
Mickaël <104686379+MickaelFarre@users.noreply.github.com>
2023-01-11 11:53:09 +0100

    Ajout de la partie 5

---
+++ Exercice 5/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Partie 7 exercice 5</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <label for="civilite">Civilité :</label>
+        <select name="civilite" id="civilite">
+            <option value="mr">Mr</option>
+            <option value="mme">Mme</option>
+        </select>
+        <br>
+        <label for="firstname">Prénom :</label>
+        <input type="text" name="firstname" id="firstname">
+        <br>
+        <label for="lastname">Nom :</label>
+        <input type="text" name="lastname" id="lastname">
+    </form>
+</body>
+</html>
